534b050922b9d8759d35175e585c88473fb68f3b
Michael Eggers <mdeggers84@gmail.com>
2017-09-15 19:54:04 -0400

    serving basic html

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+// =============================================================
+
+// Set up the Express app
+var app = express();
+var PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+// Sets up the Express app to handle data parsing (for POST requests)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// Path for static content
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Import routes and give the server access to them.
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
+
+// =============================================================
+// Starts the server listening
+
+app.listen(PORT, IP, function () {
+  console.log('Life organization happening on PORT ' + PORT + '!');
+});
