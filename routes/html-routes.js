33519e11a217f12c3784ea3817c0ea79e45d996f
Michael Eggers <mdeggers84@gmail.com>
2017-09-18 23:05:09 -0400

    updates

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,6 @@
 module.exports = function (app, path) {
   app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.send('hello');
   });
   app.get('/hello', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/hello.html'));
