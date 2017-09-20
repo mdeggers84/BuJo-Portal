9790aa23e42c683dea86991588d3a390c0cbcfee
Michael Eggers <mdeggers84@gmail.com>
2017-09-20 18:58:27 -0400

    testing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,8 @@
 module.exports = function (app, path) {
-  app.get('/test', function (req, res) {
-    res.send('hello');
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/hello.html'));
   });
   app.get('/hello', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/hello.html'));
-  })
+  });
 };
